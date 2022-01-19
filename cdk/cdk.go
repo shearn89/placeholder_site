be8bd69ac57d0e897c230b087c808fd8d27a342a
Alex Shearn <ajshearn@amazon.co.uk>
2022-01-19 17:14:08 +0000

    Fixing checkov changes

--- cdk/cdk.go
+++ cdk/cdk.go
@@ -28,6 +28,8 @@ var (
 	// Name for the bucket/zone/apex record
 	siteName    = jsii.String("ajshearn.com")
 	domainNames = jsii.Strings("ajshearn.com", "www.ajshearn.com")
+	// logging bucket
+	logBucketName = jsii.String("ajshearn-bucket-access-logs")
 )
 
 func wrapName(extension string) *string {
@@ -45,11 +47,26 @@ func NewCFDistributionStack(scope constructs.Construct, id string, props *CdkSta
 	createdArn := jsii.String(fmt.Sprintf("arn:aws:acm:us-east-1:%s:certificate/%s", *sprops.Env.Account, certUuid))
 	certificate := awscertificatemanager.Certificate_FromCertificateArn(stack, wrapName("certificate"), createdArn)
 
+	// import log bucket
+	logBucket := awss3.Bucket_FromBucketName(stack, wrapName("logBucket"), logBucketName)
+
 	// S3 bucket for storing static website
 	hostingBucket := awss3.NewBucket(stack, wrapName("bucket"), &awss3.BucketProps{
-		BlockPublicAccess: awss3.BlockPublicAccess_BLOCK_ALL(),
-		BucketName:        siteName,
-		Encryption:        awss3.BucketEncryption_S3_MANAGED,
+		BlockPublicAccess:      awss3.BlockPublicAccess_BLOCK_ALL(),
+		BucketName:             siteName,
+		Encryption:             awss3.BucketEncryption_S3_MANAGED,
+		Versioned:              jsii.Bool(true),
+		ServerAccessLogsBucket: logBucket,
+		ServerAccessLogsPrefix: jsii.String("/s3/placeholder_bucket/"),
+		LifecycleRules: &[]*awss3.LifecycleRule{
+			{
+				AbortIncompleteMultipartUploadAfter: awscdk.Duration_Days(jsii.Number(1)),
+				Enabled:                             jsii.Bool(true),
+				Expiration:                          awscdk.Duration_Days(jsii.Number(30)),
+				ExpiredObjectDeleteMarker:           jsii.Bool(true),
+				NoncurrentVersionExpiration:         awscdk.Duration_Days(jsii.Number(7)),
+			},
+		},
 	})
 	// won't need the bucket without the distribution
 	hostingBucket.ApplyRemovalPolicy(awscdk.RemovalPolicy_DESTROY)
@@ -71,6 +88,8 @@ func NewCFDistributionStack(scope constructs.Construct, id string, props *CdkSta
 		Comment:           jsii.String("CDN for the placeholder site"),
 		DefaultRootObject: jsii.String("index.html"),
 		DomainNames:       domainNames,
+		LogBucket:         logBucket,
+		LogFilePrefix:     jsii.String("/cloudfront/placeholder_site/"),
 	})
 	// wrap as a target for the r53 record
 	target := awsroute53targets.NewCloudFrontTarget(distribution)
--- cdk/cdk_test.go
+++ cdk/cdk_test.go
@@ -1,21 +1,40 @@
 package main
 
 import (
+	"github.com/aws/jsii-runtime-go"
 	"testing"
 
 	"github.com/aws/aws-cdk-go/awscdk/v2"
 	assertions "github.com/aws/aws-cdk-go/awscdk/v2/assertions"
 )
 
+func testEnv() *awscdk.Environment {
+	return &awscdk.Environment{
+		Account: jsii.String("000000000000"),
+		Region:  jsii.String("eu-west-1"),
+	}
+}
+
 func TestCdkStack(t *testing.T) {
 	// GIVEN
 	app := awscdk.NewApp(nil)
 
 	// WHEN
-	stack := NewCFDistributionStack(app, "MyStack", nil)
+	stack := NewCFDistributionStack(app, "MyStack", &CdkStackProps{
+		awscdk.StackProps{
+			Env: testEnv(),
+			Tags: &map[string]*string{
+				"cdk":       jsii.String("true"),
+				"namespace": jsii.String(NAMESPACE),
+			},
+		},
+	})
 
 	// THEN
 	assertions.Template_FromStack(stack)
+
+	// TODO: cdk-nag
+	// awscdk.Aspects_Of(app).Add( // some aspect here)
 
 	// template.HasResourceProperties(jsii.String("AWS::S3::Bucket"), map[string]interface{}{
 	// 	"BucketName": "ajshearn.com",
